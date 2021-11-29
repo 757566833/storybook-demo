d54f3d7ea1cc447ed8c6b4fda0ca7ee23fe01450
zhj <75756683@qq.com>
2021-11-29 23:09:44 +0800

    demo

---
+++ stories/index.tsx
@@ -0,0 +1,28 @@
+import React, {cloneElement, ReactElement, useCallback, useMemo, useRef, useState} from 'react';
+export interface ColorPickerProps{
+    value?:string;
+    defaultValue?:string;
+    onChange?:(value:string)=>void;
+    children?:ReactElement
+}
+export const ColorPicker:React.FC<ColorPickerProps>=(props)=>{
+    const {value, defaultValue, onChange, children} = props;
+    const [_value, set_value] = useState<string|undefined>(defaultValue);
+    const currentValue = useMemo(()=>value||_value, [value, _value]);
+    const handleChange = useCallback((e:React.ChangeEvent<HTMLInputElement>)=>{
+        set_value(e.target.value);
+        onChange&&onChange(e.target.value);
+    }, [onChange]);
+    const display = useMemo(()=>children?{display: 'none'}:{display: 'block'}, [children]);
+
+    const colorRef = useRef<HTMLInputElement>(null);
+    const childrenClick = useCallback(()=>{
+        colorRef.current?.click();
+    }, []);
+    const memoChildren = useMemo(()=>children?cloneElement(children, {onClick: childrenClick}):undefined, [children, childrenClick]);
+    return <>
+        <input ref={colorRef} type={'color'} style={display} value={currentValue} onChange={handleChange}/>
+        {memoChildren}
+    </>;
+};
+export default ColorPicker;
